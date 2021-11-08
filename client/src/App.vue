0f0e792ab221275e9f0572147c94ecaa495fdf77
billzhou2008 <18601137342@163.com>
2021-11-08 22:18:46 +0800

    add connType-CLOSE

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <form :action="sendMessage" @click.prevent="onSubmit">
-    <input v-model="tableID" type="text" >
-    <input v-model="userID" type="text" >
-    <input v-model="betvol" type="text" >
+    <label for="tableID">TableID: </label>
+    <input v-model="tableID" type="text" > <br>
+    <label for="userID">UserID:  </label>
+    <input v-model="userID" type="text" > <br>
+    <label for="seatID">SeatID:  </label>
+    <input v-model="seatID" type="text" > <br>
+    <label for="betvol">BetVol:   </label>
+    <input v-model="betvol" type="text" > <br><br>
     <input type="submit" value="Send" @click="sendMessage">
   </form>
   <!-- <p>
@@ -17,33 +22,38 @@
       {{ rcvMessage }}
     </p>
   </div>
+  <div class="closews">
+    <button @click="WebSocketClose">CloseWS</button>
+  </div>
 </template>
 
 <script>
 let msg = {
-  "tableID": "500001",
+  "tableID": 0,
   "userID": "c63p432n1fdk5k0aeta0",
+  "seatID": 0,
   "connType": 'JOIN',
   "status": 'WAITING',
   "betvol": 200,
-  "greeting": '',
+  "greeting": 'Hi',
 }
 
 export default {
   name: 'App',
   data() {
     return {
-      betvol: 0,
+      betvol: 50,
       socket: null,
       rcvMessage: "",
       showMsg: true,
-      tableID: "",
-      userID: "",
+      tableID: 0,
+      userID: "c63p432n1fdk5k0aeta1",
+      seatID: 0,
       userStatus: ""
     }
   },
   mounted() {
-    this.socket = new WebSocket("ws://localhost:9080/ws")
+    this.socket = new WebSocket("ws://140.143.149.188:9080/ws")
     this.socket.onclose = () => {
       console.log("Connection closed")
     }
@@ -62,8 +72,9 @@ export default {
   },
   methods: {
     sendMessage() {
-      msg.tableID = this.tableID
+      msg.tableID = parseInt(this.tableID) 
       msg.userID = this.userID
+      msg.seatID = parseInt(this.seatID)
       msg.connType = "ONLINE"
       msg.status = "BETDONE"
       msg.betvol = parseInt(this.betvol)
@@ -72,9 +83,20 @@ export default {
     acceptMsg(evt) {
       let rcvJson
       this.rcvMessage = evt.data
-      rcvJson = JSON.parse(evt.data)
-      // console.log(rcvJson)
-      console.log(rcvJson.tableID, rcvJson.userID)
+      try {
+        rcvJson = JSON.parse(evt.data)
+        // console.log(rcvJson)
+        console.log(rcvJson.tableID, rcvJson.userID)
+      } catch(e) {
+        console.log(e.message)
+      }
+    },
+    WebSocketClose() {
+      msg.tableID = parseInt(this.tableID) 
+      msg.userID = this.userID
+      msg.connType = "CLOSE"
+      this.socket.send(JSON.stringify(msg))
+      this.socket.close()
     }
   }
 }
